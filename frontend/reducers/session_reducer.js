dcdf7035df3ff5b09fd3e757a06a24321e028a33
vinit_parikh <vinitp94@gmail.com>
2017-01-09 19:24:10 -0800

    Write out session reducers

---
+++ frontend/reducers/session_reducer.js
@@ -0,0 +1,22 @@
+import { RECEIVE_CURRENT_USER, RECEIVE_ERRORS } from '../actions/session_actions';
+import merge from 'lodash/merge';
+
+const _nullState = Object.freeze({
+  currentUser: null,
+  errors: []
+});
+
+const SessionReducer = (state = _nullState, action) => {
+  Object.freeze(state);
+
+  switch(action.type) {
+    case RECEIVE_CURRENT_USER:
+      return merge({}, _nullState, { currentUser: action.currentUser });
+    case RECEIVE_ERRORS:
+      return merge({}, _nullState, { errors: action.errors });
+    default:
+      return state;
+  }
+};
+
+export default SessionReducer;
