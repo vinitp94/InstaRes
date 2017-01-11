844de8d51905d1394e01f8cc83421e6574f5b808
vinit_parikh <vinitp94@gmail.com>
2017-01-11 11:05:31 -0800

    Begin restaurant model

--- app/models/restaurant.rb
+++ app/models/restaurant.rb
@@ -1,2 +1,12 @@
 class Restaurant < ApplicationRecord
+  validates :name, :address, :city, :state, :zip_code, :category,
+    :description, :price, :image_urls, :owner_id, presence: true
+  validates :price, numericality: { greater_than: 0, less_than: 6 }
+
+  belongs_to(
+    :owner,
+    class_name: :User,
+    foreign_key: :owner_id,
+    primary_key: :id
+  )
 end
