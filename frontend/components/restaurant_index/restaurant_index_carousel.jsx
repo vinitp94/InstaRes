18f624354ea01261936b2c97a65b4a7e0ba3bbd2
vinit_parikh <vinitp94@gmail.com>
2017-01-12 11:28:14 -0800

    Style arrows and restaurant index list

--- frontend/components/restaurant_index/restaurant_index_carousel.jsx
+++ frontend/components/restaurant_index/restaurant_index_carousel.jsx
@@ -1,4 +1,5 @@
 import React from 'react';
+import { Link } from 'react-router';
 import Slider from 'react-slick';
 
 class RestaurantIndexCarousel extends React.Component {
@@ -6,48 +7,27 @@ class RestaurantIndexCarousel extends React.Component {
     let settings = {
       dots: true,
       infinite: true,
-      speed: 800,
+      speed: 500,
       fade: true,
       slidesToShow: 1,
-      slidesToScroll: 1
+      slidesToScroll: 1,
+      draggable: false 
     };
 
     return (
-        <Slider {...settings}>
-          {
-            this.props.images.map(img => (
-              <div><img src={img} /></div>
-            ))
-          }
-        </Slider>
+      <Slider {...settings}>
+        {
+          this.props.images.map(img => (
+            <div className='carousel-image-container'>
+              <Link to={`/`}>
+                <img src={img} />
+              </Link>
+            </div>
+          ))
+        }
+      </Slider>
     );
   }
 }
 
 export default RestaurantIndexCarousel;
-
-// var React = require('react');
-// var Slider = require('react-slick');
-//
-// var RestaurantIndexCarousel = React.createClass({
-//   render: function () {
-//     var settings = {
-//       dots: true,
-//       infinite: true,
-//       speed: 500,
-//       slidesToShow: 1,
-//       slidesToScroll: 1
-//     };
-//     return (
-//       <Slider {...settings}>
-//         <div><h3>1</h3></div>
-//         <div><h3>2</h3></div>
-//         <div><h3>3</h3></div>
-//         <div><h3>4</h3></div>
-//         <div><h3>5</h3></div>
-//         <div><h3>6</h3></div>
-//       </Slider>
-//     );
-//   }
-// });
-// export default RestaurantIndexCarousel;
--- frontend/components/restaurant_index/restaurant_index_item.jsx
+++ frontend/components/restaurant_index/restaurant_index_item.jsx
@@ -1,14 +1,13 @@
 import React from 'react';
-import { Link } from 'react-router';
 import RestaurantIndexCarousel from './restaurant_index_carousel';
 
 // TODO: Add 'add review' and 'book table' buttons
 
 const RestaurantIndexItem = ({ restaurant }) => (
-  <li className='restaurant_index_item'>
-    <Link id='container' to={`/restaurants`}>
+  <li className='restaurant-index-item'>
+    <div className='carousel-container' to={`/restaurants`}>
         <RestaurantIndexCarousel images={restaurant.image_urls}/>
-    </Link>
+    </div>
     <div className='restaurant-index-caption'>
       <p>{restaurant.name}</p>
       <p>{restaurant.category}</p>
