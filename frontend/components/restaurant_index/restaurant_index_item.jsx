7d1144101d0c839964292fb4afd006e6b12a51f0
vinit_parikh <vinitp94@gmail.com>
2017-01-13 16:37:31 -0800

    Refactor Restaurant index item component

--- frontend/components/restaurant_index/restaurant_index_item.jsx
+++ frontend/components/restaurant_index/restaurant_index_item.jsx
@@ -1,19 +1,31 @@
 import React from 'react';
 import RestaurantIndexCarousel from './restaurant_index_carousel';
 
-const RestaurantIndexItem = ({ restaurant }) => (
-  <li className='restaurant-index-item'>
-    <div className='carousel-container'>
-        <RestaurantIndexCarousel
-          images={restaurant.image_urls}
-          restaurantId={restaurant.id}/>
-    </div>
-    <div className='restaurant-index-caption'>
-      <p>{restaurant.name}</p>
-      <p>{restaurant.category}</p>
-      <p>{restaurant.price}</p>
-    </div>
-  </li>
-);
+class RestaurantIndexItem extends React.Component {
+  priceToSymbol(price) {
+    let symbol = [];
+    for(let i = 0; i < price; i++) {
+      symbol.push('$');
+    }
+    return symbol.join('');
+  }
+
+  render () {
+    return (
+      <li className='restaurant-index-item'>
+        <div className='carousel-container'>
+            <RestaurantIndexCarousel
+              images={this.props.restaurant.image_urls}
+              restaurantId={this.props.restaurant.id}/>
+        </div>
+        <div className='restaurant-index-caption'>
+          <p>{this.props.restaurant.name}</p>
+          <p>{this.props.restaurant.category}</p>
+          <p>{this.priceToSymbol(this.props.restaurant.price)}</p>
+        </div>
+      </li>
+    );
+  }
+}
 
 export default RestaurantIndexItem;
