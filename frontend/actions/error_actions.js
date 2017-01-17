12d385940f66ab88e3e2e02a550d975217d964a8
vinit_parikh <vinitp94@gmail.com>
2017-01-16 17:47:37 -0800

    Update error actions and error reducer

--- frontend/actions/error_actions.js
+++ frontend/actions/error_actions.js
@@ -1,7 +1,9 @@
 import { receiveSessionErrors } from './session_actions';
 import { receiveRestaurantErrors } from './restaurant_actions';
+import { receiveReviewErrors } from './review_actions';
 
 export const clearErrors = (dispatch) => {
   dispatch(receiveSessionErrors());
   dispatch(receiveRestaurantErrors());
+  dispatch(receiveReviewErrors());
 };
--- frontend/reducers/errors_reducer.js
+++ frontend/reducers/errors_reducer.js
@@ -1,10 +1,12 @@
 import { RECEIVE_SESSION_ERRORS } from '../actions/session_actions';
 import { RECEIVE_RESTAURANT_ERRORS } from '../actions/restaurant_actions';
+import { RECEIVE_REVIEW_ERRORS } from '../actions/review_actions';
 import merge from 'lodash/merge';
 
 const _defaultState = {
   sessionErrors: [],
-  restaurantErrors: []
+  restaurantErrors: [],
+  reviewErrors: []
 };
 
 const ErrorsReducer = (state = _defaultState, action) => {
@@ -14,6 +16,8 @@ const ErrorsReducer = (state = _defaultState, action) => {
       return merge({}, _defaultState, { sessionErrors: action.errors });
     case RECEIVE_RESTAURANT_ERRORS:
       return merge({}, _defaultState, { restaurantErrors: action.errors });
+    case RECEIVE_REVIEW_ERRORS:
+      return merge({}, _defaultState, { reviewErrors: action.errors });
     default:
       return state;
   }
