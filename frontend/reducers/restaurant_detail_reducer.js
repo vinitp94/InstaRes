32149d23c4cc58de8684b9372c5176cdd96f079a
Vinit <vinitp94@gmail.com>
2017-02-15 14:27:50 -0800

    Remove unneccesary cases in detail reducer

--- frontend/reducers/restaurant_detail_reducer.js
+++ frontend/reducers/restaurant_detail_reducer.js
@@ -1,6 +1,6 @@
-import { RECEIVE_RESTAURANT, REMOVE_RESTAURANT } from '../actions/restaurant_actions';
-import { RECEIVE_REVIEW, REMOVE_REVIEW } from '../actions/review_actions';
-import { RECEIVE_RESERVATION, REMOVE_RESERVATION } from '../actions/reservation_actions';
+import { RECEIVE_RESTAURANT } from '../actions/restaurant_actions';
+import { RECEIVE_REVIEW } from '../actions/review_actions';
+import { RECEIVE_RESERVATION } from '../actions/reservation_actions';
 import { merge } from 'lodash';
 
 const RestaurantDetailReducer = (state = {}, action) => {
@@ -9,8 +9,6 @@ const RestaurantDetailReducer = (state = {}, action) => {
   switch(action.type) {
     case RECEIVE_RESTAURANT:
       return merge({}, action.restaurant);
-    case REMOVE_RESTAURANT:
-      return {};
     case RECEIVE_REVIEW:
       let restCopy = merge({}, state);
       restCopy.ave_rating = action.review.ave_rating;
@@ -22,28 +20,12 @@ const RestaurantDetailReducer = (state = {}, action) => {
         created_at: action.review.created_at }});
       restCopy.reviews = newReviews;
       return restCopy;
-    case REMOVE_REVIEW:
-      if (Object.keys(state).length > 0) {
-        let copyRest = merge({}, state);
-        copyRest.ave_rating = action.review.ave_rating;
-        copyRest.num_reviews = action.review.num_reviews;
-        delete copyRest.reviews[action.review.id];
-        return copyRest;
-      }
-      return state;
     case RECEIVE_RESERVATION:
       let newReserve = merge({}, state.reservations, { [action.reservation.id]: {
         id: action.reservation.id,
         party_size: action.reservation.party_size,
         slot: action.reservation.slot }});
       return merge({}, state, { reservations: newReserve });
-    case REMOVE_RESERVATION:
-      if (Object.keys(state).length > 0) {
-        let copyReserve = merge({}, state);
-        delete copyReserve.reservations[action.reservation.id];
-        return copyReserve;
-      }
-      return state;
     default:
       return state;
   }
