c6eaee0fe087a9905e6f9eb819aa2965d7fd1e59
Vinit <vinitp94@gmail.com>
2017-02-15 16:33:01 -0800

    Change loading spinner type

--- frontend/components/spinner.jsx
+++ frontend/components/spinner.jsx
@@ -4,7 +4,7 @@ class Spinner extends React.Component {
   render() {
     return (
       <div className="loading">
-        <i className="fa fa-spinner fa-spin fa-3x fa-fw" aria-hidden="true"></i>
+        <i className="fa fa-spinner fa-pulse fa-3x fa-fw" aria-hidden="true"></i>
       </div>
     );
   }
